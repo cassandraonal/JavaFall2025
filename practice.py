bed9498ce6f0ff18cdd866c352570aca7318d5bc
Cassie-Sboroni <cassandra.sbordoni96@student.qcc.cuny.edu>
2025-02-18 16:19:21 +0000

    final version: using input

--- practice.py
+++ practice.py
@@ -8,3 +8,9 @@ a, b = "hi", "python"
 print(a, '\n', b, sep = "") #works
 print(a, b, sep = "\n") #works
 print(a+'\n'+b) #stacks right on top of eachother prints same as 2 above.
+
+#input allows the user to input something they want anything, to be displayed.
+a=input("Enter a greeting: ") #you can do with no spacing 
+b = input("Enter some text: ") #or with.
+
+print(a.capitalize()+', '+b.lower()+"!")
