c9046cf72fbcbab46d1f1f0c66fc2f119dc803f4
Cassie-Sboroni <cassandra.sbordoni96@student.qcc.cuny.edu>
2025-02-18 16:11:22 +0000

    2nd stage: using variables

--- practice.py
+++ practice.py
@@ -2,3 +2,9 @@
 #Cassandra Sbordoni
 
 print("Hello Python world!")
+
+a, b = "hi", "python"
+#print(a, '\n', b) prints extra space, so no good.
+print(a, '\n', b, sep = "") #works
+print(a, b, sep = "\n") #works
+print(a+'\n'+b) #stacks right on top of eachother prints same as 2 above.
