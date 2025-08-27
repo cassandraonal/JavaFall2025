7ee2c2f6c562345ff31895c940365e554ace1ac7
Cassandra Onal <cassandra.sbordoni96@student.qcc.cuny.edu>
2025-08-27 19:49:35 +0000

    firstday_Java2025

---
+++ Cassie.java
@@ -0,0 +1,6 @@
+public class Cassie{
+    public static void main(String[] args){
+        System.out.println("My name is Cassandra");
+        System.out.println("My age is 26");
+    }
+}
---
+++ Ex1.java
@@ -0,0 +1,5 @@
+public class Ex1{
+    public static void main(String[] args){
+        System.out.println("Name\tage\nAmy\t24\nKate\t18\nJack\t19");
+    }
+}
