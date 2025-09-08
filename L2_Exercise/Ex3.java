83a684bfa1e0a8f3bee536055cfe72b2cf5d3b88
Cassandra Onal <cassandra.sbordoni96@student.qcc.cuny.edu>
2025-09-08 19:49:25 +0000

    Ex3

---
+++ L2_Exercise/Ex3.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Ex3 {
+    public static void main(String [] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print(" 'c' for Cheeseburger\n'b' for beef burger\n'f' for franch fry\n'w' for water");
+        System.out.print("Enter your choice: ");
+        char option = input.next().charAt(0);
+        switch(option){
+            case 'c':
+                System.out.println("You chose the cheeseburger");
+                break;
+            case 'b':
+                System.out.println("You chose the beef burger");
+                break;
+            case 'f':
+                System.out.println("You chose french fries");
+                break;
+            case 'w':
+                System.out.println("You chose water");
+                break;
+            default:
+                System.out.println("Invalid option");
+
+        }
+    }
+}
