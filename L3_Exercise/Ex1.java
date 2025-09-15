a07446da2a9807cf7a88c2dead68dc800b03da34
Cassandra Onal <cassandra.sbordoni96@student.qcc.cuny.edu>
2025-09-15 18:16:06 +0000

    fixed_L3_Ex1

--- L3_Exercise/Ex1.java
+++ L3_Exercise/Ex1.java
@@ -2,9 +2,9 @@ public class Ex1{
     public static void main(String [] args){
         // while loop
         int i = 0; //initializeation
-        while(1 < 5){ //comparision
-            System.out.println(1);
-            i++; //update: self increment (i++ <=> i += 1)
+        while(i < 5){ //comparision <-- print i 5 times. 
+            System.out.println(i);
+            i++; //update: self increment (i++ <=> i += 1) <--- icreases i each run
         }
     }
 }
