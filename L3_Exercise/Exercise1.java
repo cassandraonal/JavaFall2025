8994fc94d1eb6837079912de608ebb920835ac17
Cassandra Onal <cassandra.sbordoni96@student.qcc.cuny.edu>
2025-09-15 18:55:24 +0000

    Ex6_L3

--- L3_Exercise/Exercise1.java
+++ L3_Exercise/Exercise1.java
@@ -38,13 +38,31 @@ public class Exercise1 {
         }
         System.out.println(r);
         System.out.println();
-        int r1 = 0;
-        int i5 = 1;
-        while(i5<=100){
+        int r1 = 0; // result of sum starting at 0.
+        int i5 = 1; //counter
+        while(i5<=100){ //condition to add up to 100
             r1 = r1 + i5;
             i5++;
         }
         System.out.println(r1);
+        System.out.println();
+        int r2 = 0; // result of sum starting at 0.
+        int i6 = 2; //counter
+        while(i6<=100){ //condition to add up to 100
+            r2 = r2 + i6;
+            i6+=2;
+        }
+        System.out.println(r2);
+        System.out.println();
+        //Ex6 : calculate the factorial 10 use while
+        //10! = 10 * 9* 8 * 7... * 1 = 3628800
+        int r3 = 1; // result of sum starting at 1. for multiplication
+        int i7 = 10; //counter
+        while(i7>= 1){ //condition
+            r3 = r3 * i7;
+            i7--; // decreasing for going from 10 down to 1.
+        }
+        System.out.println(r3);
     }
 }
 
