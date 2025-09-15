cb4286ed4b7377aa8a7d673b1576e0e0d5fd02fc
Cassandra Onal <cassandra.sbordoni96@student.qcc.cuny.edu>
2025-09-15 18:31:22 +0000

    L3_ex1

--- L3_Exercise/Exercise1.java
+++ L3_Exercise/Exercise1.java
@@ -5,5 +5,31 @@ public class Exercise1 {
             System.out.println(i1+" ");
             i1++; //update
         }
+        System.out.println();
+        // print out 100 to 200
+        int i2 = 100;
+        while(i2 <= 200){
+            System.out.println(i2+" ");
+            i2++;
+        }
+        System.out.println();
+        // Print all the even number from 1 to 100
+        // 2 4 6 8 10 12 ... 98 100
+        int i3 = 0;
+        while(i3 <= 100){
+            if(i3%2==0){
+              System.out.println(i3+" ");
+            }
+            i3++;
+        }
+        System.out.println();
+        //second approach
+        int i4 = 2;
+        while(i4 <= 100){
+              System.out.println(i4+" ");
+              i4+=2; // i4 = i4 + 2;
+        }
     }
 }
+
+   
