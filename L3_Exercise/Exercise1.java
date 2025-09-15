b775fd8d3217ff599fe248b0484e85fe9ecd2be6
Cassandra Onal <cassandra.sbordoni96@student.qcc.cuny.edu>
2025-09-15 18:18:34 +0000

    Exercise1_L3

---
+++ L3_Exercise/Exercise1.java
@@ -0,0 +1,9 @@
+public class Exercise1 {
+    public static void main(String[] args){
+        int i1 = 1;
+        while(i1 <= 100){ // comparison
+            System.out.println(i1+" ");
+            i1++; //update
+        }
+    }
+}
