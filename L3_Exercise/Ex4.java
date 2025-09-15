5a7b77f29c83b8fdfdde83a7730b9ad5a1e56fd0
Cassandra Onal <cassandra.sbordoni96@student.qcc.cuny.edu>
2025-09-15 20:47:34 +0000

    examples_made

---
+++ L3_Exercise/Ex4.java
@@ -0,0 +1,20 @@
+public class Ex4 {
+     public static void main(String[] args) {
+        for(int i = 1; i <=100; i++){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+        // Ex 2: print 100 to 200
+        for(int i = 100; i <=200; i++){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+        //Ex 3: Print all the even number from 1 to 100
+        for(int i = 2; i<=100; i+=2){
+            System.out.print(i+" ")
+        }
+
+    }
+}
+
+
