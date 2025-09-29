ddfd17c1dda2bc13a48bb106bacf15fe2eb8159b
Cassandra Onal <cassandra.sbordoni96@student.qcc.cuny.edu>
2025-09-29 19:00:00 +0000

    added teachers comments

--- L4_Exercise/Ex1.java
+++ L4_Exercise/Ex1.java
@@ -1,20 +1,31 @@
-public class Ex1 {
-    public static void main(String[] args){
-        String [] a = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
-        System.out.println(a[0]);
-        System.out.println(a[1]);
-        System.out.println(a[2]);
-        System.out.println(a[3]);
-        System.out.println(a[4]);
-        a[0] = "Mon";
-        a[1] = "Tue";
-        a[2] = "Wed";
-        a[3] = "Thu";
-        a[4] = "Fri";
-        System.out.println(a[0]);
-        System.out.println(a[1]);
-        System.out.println(a[2]);
-        System.out.println(a[3]);
-        System.out.println(a[4]);
-    }    
+public class Ex1{
+  public static void main(String[] args) {
+    /*
+    Create an array with five values
+    "Monday","Tuesday","Wednesday","Thursday","Friday"
+
+    print these element one by one
+    and update the array to
+    "Monday" => "Mon"
+    "Tuesday" => "Tue"
+    ...
+    "Friday" => "Fri"
+    */
+    String [] a = {"Monday","Tuesday","Wednesday","Thursday","Friday"};
+    System.out.println(a[0]);
+    System.out.println(a[1]);
+    System.out.println(a[2]);
+    System.out.println(a[3]);
+    System.out.println(a[4]);
+    a[0] = "Mon";
+    a[1] = "Tue";
+    a[2] = "Wed";
+    a[3] = "Thu";
+    a[4] = "Fri";
+    System.out.println(a[0]);
+    System.out.println(a[1]);
+    System.out.println(a[2]);
+    System.out.println(a[3]);
+    System.out.println(a[4]);
+  }
 }
---
+++ L4_Exercise/Ex2.java
@@ -0,0 +1,29 @@
+public class Ex1{
+  public static void main(String[] args) {
+    /*
+    Create an array with five values
+    "Monday","Tuesday","Wednesday","Thursday","Friday"
+    print these element use for loop
+    */
+    String [] a = {"Monday","Tuesday","Wednesday","Thursday","Friday"};
+    for(int i=0;i<a.length;i++)
+        System.out.println(a[i]);
+    /* Create an array with 1-100, print everything out use for loop*/
+    int [] b = new int[100];
+    for(int i=0;i<b.length;i++){
+        b[i] = i+1;
+    }
+    for(int i=0;i<b.length;i++)
+        System.out.print(b[i]+" ");
+    System.out.println();
+    /*
+    Create an array with five values
+    "Monday","Tuesday","Wednesday","Thursday","Friday"
+    print these element use for-each loop
+    */
+    String [] c = {"Monday","Tuesday","Wednesday","Thursday","Friday"};
+    for(String i: c){
+        System.out.println(i);
+    }
+  }
+}
