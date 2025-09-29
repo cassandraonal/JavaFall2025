9265e62aebb552c2283312bb45dd1aac95baf54d
Cassandra Onal <cassandra.sbordoni96@student.qcc.cuny.edu>
2025-09-29 19:12:52 +0000

    Ex3

---
+++ L4_Exercise/Ex3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Ex3 {
+    public static void main(String[] args) {
+        /*Write a program that prompts 
+ * the user for each of eight values
+ * stores these in an array
+ * 1. Prints each of these out
+ * 2.Prints their total.
+ */
+        int [] a = new int[8];
+        Scanner input = new Scanner(System.in);
+        for(int i=0;i<a.length;i++){
+            System.out.println("Enter array index "+i+":");
+            a[i] = input.nextInt();
+        }
+        int sum = 0;
+         for(int i=0;i<a.length;i++){
+            System.out.print(a[i]+" ");
+            sum = sum +a[i];
+         }
+         System.out.print("\nSum:"+sum);
+        
+    }
+}
