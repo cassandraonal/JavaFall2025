805f66baebc0e2638a649d5994fa71b1482e25c7
Cassandra Onal <cassandra.sbordoni96@student.qcc.cuny.edu>
2025-09-29 18:36:04 +0000

    ex1

---
+++ L4_Exercise/Ex1.java
@@ -0,0 +1,20 @@
+public class Ex1 {
+    public static void main(String[] args){
+        String [] a = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+        System.out.println(a[0]);
+        System.out.println(a[1]);
+        System.out.println(a[2]);
+        System.out.println(a[3]);
+        System.out.println(a[4]);
+        a[0] = "Mon";
+        a[1] = "Tue";
+        a[2] = "Wed";
+        a[3] = "Thu";
+        a[4] = "Fri";
+        System.out.println(a[0]);
+        System.out.println(a[1]);
+        System.out.println(a[2]);
+        System.out.println(a[3]);
+        System.out.println(a[4]);
+    }    
+}
