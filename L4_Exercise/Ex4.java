3dd00eaf5238f793f1c69bd2af48740e08c60842
Cassandra Onal <cassandra.sbordoni96@student.qcc.cuny.edu>
2025-10-06 18:23:50 +0000

    Ex4

---
+++ L4_Exercise/Ex4.java
@@ -0,0 +1,16 @@
+/*
+ create a two di array which save
+ 10 11 12 13 14
+ 15 16 17 18 19 
+ 20 21 22 23 24
+ 25 26 27 28 29
+ */
+public class Ex4 {
+    public static void main(String[] args){
+        int [][] a = {{10,11,12,13,14},{15,16,17,18,19},{20,21,22,23,24},{25,26,27,28,29}};
+        System.out.println(a[0][3]);
+        System.out.println(a[1][1]);
+        System.out.println(a[2][4]);
+        System.out.println(a[3][2]);
+    }
+}
