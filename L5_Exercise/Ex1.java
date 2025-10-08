f32e10d4c97a96e66b388bc9242def4745d6bf49
Cassandra Onal <cassandra.sbordoni96@student.qcc.cuny.edu>
2025-10-08 18:19:18 +0000

    Ex1_L5

---
+++ L5_Exercise/Ex1.java
@@ -0,0 +1,10 @@
+public class Ex1{
+    public static void print_name(){
+        System.out.println("Cassandra Onal");
+    }
+    public static void main(String[] args){
+        print_name();
+        print_name();
+        print_name();
+    }
+}
