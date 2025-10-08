9891a18daad321396263843e0fc44b61de977dfc
Cassandra Onal <cassandra.sbordoni96@student.qcc.cuny.edu>
2025-10-08 19:06:24 +0000

    comment

--- L5_Exercise/Ex6.java
+++ L5_Exercise/Ex6.java
@@ -9,7 +9,7 @@ public class Ex6{
         System.out.println(multiply(1.5,2.5));
         System.out.println(multiply(4,5));
         System.out.println(multiply(10,20));
-        System.out.println(multiply(1.5,10)); // uses double because of overloading
+        System.out.println(multiply(1.5,10)); // uses double because of overloading bc int --> double is automatic, while double --> int has to be done manually.
         System.out.println(multiply(10,1.5)); // " "
     }
 }
