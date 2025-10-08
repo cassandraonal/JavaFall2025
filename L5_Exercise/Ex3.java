df5609e868cfe7e1edaddb53d8d172767b1f089d
Cassandra Onal <cassandra.sbordoni96@student.qcc.cuny.edu>
2025-10-08 18:35:24 +0000

    Ex3_L5

---
+++ L5_Exercise/Ex3.java
@@ -0,0 +1,10 @@
+public class Ex3 {
+    static void print_info(String name, int age){
+        System.out.println("Your name is "+name+", you are "+age+" years old.");
+    }    
+    public static void main(String[] args){
+        print_info("Cassandra", 26);
+        print_info("Joon", 28);
+        print_info("Happi", 3);
+    }
+}
