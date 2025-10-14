e887a9212c4ce637fc085267b8b4a1f9efc7e242
Cassandra Onal <cassandra.sbordoni96@student.qcc.cuny.edu>
2025-10-14 19:07:04 +0000

    examples

---
+++ L6_Exercise/Employee.java
@@ -0,0 +1,4 @@
+public class Employee {
+    String name="TBD";
+    int id=0;
+}
---
+++ L6_Exercise/Main.java
@@ -0,0 +1,13 @@
+class Main {
+    public static void main(String [] args){
+        Employee e1 = new Employee();
+        Employee e2 = new Employee();
+        e1.name = "Jack";
+        e1.id = 123456;
+        e2.name = "David";
+        e2.id = 888888;
+        System.out.println(e1.name +" "+e1.id);
+        System.out.println(e2.name +" "+e2.id);
+    }
+    
+}
