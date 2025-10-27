986c9eddc64d7ff8fe5d4b6c30d2db59e0cfeb95
Cassandra Onal <cassandra.sbordoni96@student.qcc.cuny.edu>
2025-10-27 19:19:12 +0000

    ok

--- Inheritence/Ex2/TestCar.java
+++ Inheritence/Ex2/TestCar.java
@@ -1,9 +1,12 @@
-public class TestCar {
+class TestCar{
     public static void main(String args[]){
-        vehicle v1 = new vehicle();
-        v1.display();
-        vehicle v2 = new vehicle("BMW", 60000);
-        v2.display();
-        Car
+        vehicle v1=new vehicle();
+        v1.display(); // brand: tbd, price: 0
+        vehicle v2=new vehicle("BMW",60000);
+        v2.display(); // brand: BMW, price: 60000
+        Car c1=new Car();
+        c1.display(); // brand: tbd, price: 0, cylinder: 0
+        Car c2=new Car("Honda",35000,4);
+        c2.display(); // brand: Honda, price: 35000, cylinder: 4
     }
 }
