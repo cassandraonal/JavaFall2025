d970b592b1dacde78743d610e845ae0276595c0a
Cassandra Onal <cassandra.sbordoni96@student.qcc.cuny.edu>
2025-11-19 20:38:42 +0000

    L10

---
+++ L10_Exercise/Ex7.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+public class Ex7 {
+    public static void main(String[] args) {
+        ArrayList<Integer> a = new ArrayList<Integer>();
+        for(int i=0; i<11; i++){
+            a.add(1+(int)(Math.random()*10));
+        }
+        System.out.println(a);
+        HashSet<Integer> s = new HashSet<Integer>();
+        for(int i=0;i<a.size();i++){
+            s.add(a.get(i));
+        }
+        System.out.println(s.size());
+
+    }
+}
